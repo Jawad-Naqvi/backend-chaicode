473ca66ba827cc91401f449e1711ea9429a3c6e4
Jawad-Naqvi <163732363+Jawad-Naqvi@users.noreply.github.com>
2025-07-26 00:31:52 +0530

    setup configured and postman started

--- src/index.js
+++ src/index.js
@@ -3,20 +3,22 @@
 // // import constnats from "./constants.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
-
+// import express from "express";
+import {app} from "./app.js";
 
 
 dotenv.config({
     path: './env'
 })
 
+// const app = express();
 
 
 connectDB()
 .then(() => {
-  application.liste(process.env.PORT || 8000, () => {
-    console.log(`Server is running at port: ${process.env.PORT}`)
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at port: ${process.env.PORT}`);
+    // console.log(`Server is running at port: ${process.env.PORT} || 8000`);  agar mai import karu express ko to mujhe  const app = express() likna padega aur || 8000 likhne ki zarurut hai mongoDB connect karna ka liye 
   })
 })
 .catch((err) => {
